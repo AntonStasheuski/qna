8f8243b3deda07cf203cb170076f8781808b82c0
Anton <admin@MacBook-Pro-Admin.local>
2020-09-21 18:58:13 +0300

    new | edit | create | update | destroy || for Questions controller

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  let(:question) { create(:question) }
+
   describe 'GET #index' do
     let(:questions) { create_list(:question, 3) }
 
@@ -18,8 +20,6 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'GET #show' do
-    let(:question) { create(:question) }
-
     before { get :show, params: { id: question } }
 
     it 'assigns the requested question to @question' do
@@ -28,6 +28,99 @@ RSpec.describe QuestionsController, type: :controller do
 
     it 'render show view' do
       expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #new' do
+    before { get :new }
+
+    it 'assigns a new question to @question' do
+      expect(assigns(:question)).to be_a_new Question
+    end
+
+    it 'render new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'GET #edit' do
+    before { get :edit, params: { id: question } }
+
+    it 'assigns the requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'render show view' do
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'save a new question' do
+        expect { post :create, params: { question: attributes_for(:question) } }.to change(Question, :count).by(1)
+      end
+      it 'redirect to show view' do
+        post :create, params: { question: attributes_for(:question) }
+        expect(response).to redirect_to assigns(:question)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the question' do
+        expect { post :create, params: { question: attributes_for(:question, :invalid) } }.to_not change(Question, :count)
+      end
+      it 're-render new view' do
+        post :create, params: { question: attributes_for(:question, :invalid) }
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+  describe 'PATCH #update' do
+    context 'with valid attributes' do
+      it 'assigns the requested question to @question' do
+        patch :update, params: { id: question, question: attributes_for(:question) }
+        expect(assigns(:question)).to eq question
+      end
+      it 'change question attributes' do
+        patch :update, params: { id: question, question: { title: 'title2', body: 'body2' } }
+        question.reload
+
+        expect(question.title).to eq 'title2'
+        expect(question.body).to eq 'body2'
+      end
+      it 'redirect to  updated question' do
+        patch :update, params: { id: question, question: attributes_for(:question) }
+        expect(response).to redirect_to question
+      end
+    end
+
+    context 'with invalid attributes' do
+      before { patch :update, params: { id: question, question: attributes_for(:question, :invalid) } }
+      it 'does not change question attributes' do
+        question.reload
+
+        expect(question.title).to eq 'MyTitle'
+        expect(question.body).to eq 'MyText'
+      end
+
+      it 're-render edit view' do
+        expect(response).to render_template :edit
+      end
+    end
+
+    describe 'DELETE #destroy' do
+      let!(:question) { create(:question) }
+
+      it 'delete the question' do
+        expect { delete :destroy, params: { id: question } }.to change(Question, :count).by(-1)
+      end
+
+      it 'redirects to index' do
+        delete :destroy, params: { id: question }
+        expect(response).to redirect_to question_path
+      end
     end
   end
 end
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :question do
     title { 'MyTitle' }
     body { 'MyText' }
+
+    trait :invalid do
+      title { nil }
+    end
   end
 end

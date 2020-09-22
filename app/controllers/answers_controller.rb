b48a77901d526b4d4ccb490fc22c55a469f49c7e
AntonStashevski <antonstashevski16@gmail.com>
2020-09-22 13:58:15 +0300

    answer show

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -4,9 +4,26 @@ class AnswersController < ApplicationController
     @answers = question.answers.all
   end
 
+  def show
+
+  end
+
+  def create
+    @answer = question.answers.new(answer_params)
+    if @answer.save
+      # redirect_to @question
+    else
+      # render :new
+    end
+  end
+
   private
 
   helper_method :question
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
 
   def question
     @question ||= Question.find(params[:question_id])
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -16,4 +16,39 @@ RSpec.describe AnswersController, type: :controller do
     end
   end
 
+  describe 'GET #show' do
+    let(:answer) { create(:answer) }
+
+    before { get :show, params: { question_id: answer.question, id: answer } }
+
+
+    it 'render show view' do
+      expect(response).to render_template :show
+    end
+  end
+
+  describe 'POST #create' do
+    let(:question) { create(:question) }
+
+    context 'with valid attributes' do
+      it 'save a new answer' do
+        expect { post :create, params: { question_id: question, answer: attributes_for(:answer) } }.to change(Answer, :count).by(1)
+      end
+      # it 'redirect to show view' do
+      #   post :create, params: { question_id: question, answer: attributes_for(:answer) }
+      #   expect(response).to redirect_to assigns(:answer)
+      # end
+    end
+
+    # context 'with invalid attributes' do
+    #   it 'does not save the question' do
+    #     expect { post :create, params: { question: attributes_for(:question, :invalid) } }.to_not change(Question, :count)
+    #   end
+    #   it 're-render new view' do
+    #     post :create, params: { question: attributes_for(:question, :invalid) }
+    #     expect(response).to render_template :new
+    #   end
+    # end
+  end
+
 end

e5c71610635fae4881f70055dda15237f8f9213d
AntonStashevski <antonstashevski16@gmail.com>
2020-10-18 20:25:05 +0300

    user can attach files to answer *create

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,8 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
-  belongs_to :user
-  validates :title, :body, presence: true
-
   has_many_attached :files
 
+  belongs_to :user
+
+  validates :title, :body, presence: true
 end
--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -27,6 +27,25 @@ feature 'User can create answer', "
 
       expect(page).to have_content "Body can't be blank"
     end
+
+    scenario 'tries to ask a answer with attached file' do
+      fill_in 'Body', with: 'body1'
+
+      attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+      click_on 'Answer'
+
+      expect(page).to have_link "rails_helper.rb"
+    end
+
+    scenario 'tries to ask a answer with attached files' do
+      fill_in 'Body', with: 'body1'
+
+      attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+      click_on 'Answer'
+
+      expect(page).to have_link "rails_helper.rb"
+      expect(page).to have_link "spec_helper.rb"
+    end
   end
 
   describe 'Unauthenticated user' do
